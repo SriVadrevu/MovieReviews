f7818f5fbbc41dcdf676c7ad1859439220aaa133
Sri Vadrevu <srivadrevu@sris-macbook-air.local>
2016-11-09 11:12:51 -0600

    Initial Commit

---
+++ Project3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project3
+//
+//  Created by Sri Vadrevu on 11/9/16.
+//  Copyright © 2016 SriVadrevu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
